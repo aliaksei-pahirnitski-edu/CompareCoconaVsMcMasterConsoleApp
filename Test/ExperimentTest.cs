a85111527823f3acff7114c8fa1f502be9b9f8f1
aliaksei.pahirnitski.edu <aliaksei.pahirnitski.edu@gmail.com>
2022-06-24 10:20:13 +0300

    Add test project

---
+++ Test/ExperimentTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Test
+{
+    public class ExperimentTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
